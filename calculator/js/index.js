c4276b765fca425ea5d1f3ba0a25cee551f2cd31
BenjaminAJ <ajanakub30@gmail.com>
2023-02-28 09:02:04 +0100

    modified functions

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -1,27 +1,28 @@
-function clearScreen(){
-    document.getElementById('output').innerText= ''
+function clearScreen() {
+    document.getElementById("output").innerText = "0";
 }
 
-if (document.getElementById('output').innerText == ''){
-    document.getElementById('output').innerText= '0'
+if (document.getElementById("output").innerText == "") {
+    document.getElementById("output").innerText = "0";
 }
-function outputValue(value){
-    if (document.getElementById('output').innerText != '0'){
-        document.getElementById('output').innerText += value
-    }
-    else{
-        document.getElementById('output').innerText = value
+function outputValue(value) {
+    if (document.getElementById("output").innerText != "0") {
+        document.getElementById("output").innerText += value;
+    } else {
+        document.getElementById("output").innerText = value;
     }
 }
-function square(){
-    if(document.getElementById('output').innerText == '0'){
-        document.getElementById('output').innerText= '0'
-    }
-    else{
-        let userinput=document.getElementById('output').innerText
+function square() {
+    let userinput = document.getElementById("output").innerText;
 
-        userinput = Number(userinput) ** 2
-        document.getElementById('output').innerText = userinput
-    }
+    userinput = Number(userinput) ** 2;
+    document.getElementById("output").innerText = userinput;
 }
-console.log('' == '');
+function squareRoot() {
+    let userinput = document.getElementById("output").innerText;
+
+    userinput = Number(userinput) ** 0.5;
+    document.getElementById("output").innerText = userinput;
+
+}
+console.log("" == "");
